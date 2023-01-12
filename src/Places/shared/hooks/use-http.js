616c8b15ade3cbc6f4afa2371b4cbb07ca0ef40d
shouryamishra54 <47191571+shouryamishra54@users.noreply.github.com>
2023-01-12 17:36:13 +0530

    Add files via upload

--- src/Places/shared/hooks/use-http.js
+++ src/Places/shared/hooks/use-http.js
@@ -1,1 +1,23 @@
+import { useCallback, useEffect, useRef, useState } from "react"
 
+function UseHttp(requestFunction){
+    const [isLoading, setIsLoading]=useState(false);
+    const [error, setError]=useState();
+    const [data, setData]=useState();
+    const sendRequest=useCallback(async(requestData)=>{
+        setIsLoading(true)
+        setError(null)
+        try{
+            setError(null)
+            const responseData=await requestFunction(requestData)
+            setData(responseData)
+            setIsLoading(false)
+        }catch(e){
+            const errmsg=e.message || "Something went wrong";
+            setError(errmsg);
+            setIsLoading(false);
+        }
+    })
+    return {isLoading, error, data, sendRequest}
+}
+export default UseHttp
