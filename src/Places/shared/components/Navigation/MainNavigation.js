5e9f03873777a8223d890ce5519b2080be3a0862
shouryamishra54 <47191571+shouryamishra54@users.noreply.github.com>
2023-01-12 17:32:20 +0530

    Add files via upload

--- src/Places/shared/components/Navigation/MainNavigation.js
+++ src/Places/shared/components/Navigation/MainNavigation.js
@@ -1,1 +1,27 @@
+import React, { useState } from "react";
+import { Button, Nav, Navbar, Offcanvas } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import NavLink from "./NavLink";
 
+function MainNavigation(props){
+    const [drawerOpened, setDrawerOpened]=useState(false);
+    function openDrawer(){
+        setDrawerOpened(true)
+    }
+    function closeDrawer(){
+        setDrawerOpened(false)
+    }
+    return (<React.Fragment>
+            <NavLink expand={false}/>
+        <Offcanvas show={drawerOpened} onHide={closeDrawer}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+    </React.Fragment>)
+}
+export default MainNavigation
