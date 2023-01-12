e9a78381ac5d77ffd08f2e7099d41183d6798fc1
shouryamishra54 <47191571+shouryamishra54@users.noreply.github.com>
2023-01-12 18:58:17 +0530

    Delete src/APIFetcher directory

--- src/APIFetcher/custom/Footer.js
+++
@@ -1,6 +0,0 @@
-import CustomHook from "./custom-hook";
-function Footer(){
-    const counter=CustomHook()
-    return (<div className="container"><h2>You are in Footer Counter: {counter}</h2></div>)
-}
-export default Footer;
--- src/APIFetcher/custom/Header.js
+++
@@ -1,8 +0,0 @@
-import CustomHook from "./custom-hook";
-function Header(){
-    const counter=CustomHook(true);
-    return (<div className={`modal-header dropdown-header`} style={{border:"2px solid"}}>
-        <div className="container"><h2>You are in Header Counter: {counter}</h2></div>
-    </div>)
-}
-export default Header;
